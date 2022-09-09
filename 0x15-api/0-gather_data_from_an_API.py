5f249e0f60efcbadd91b093984622177113c1f01
miguecoder <mmarqzm@gmail.com>
2022-09-09 13:32:31 -0500

    Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-Python script that, using this REST API,
-for a given employee ID, returns information
-about his/her TODO list progress.
-"""
+"""Python script that, using this REST API"""
 import requests
 from sys import argv
 
@@ -21,15 +17,12 @@ if __name__ == "__main__":
     task_completed = requests.get(all_url, params=filter).json()
     num_task_c = len(task_completed)
 
-    print("Employee {} is done with tasks({}/{}):".format(name.get("name"),
-          num_task_c, num_task))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name.get("name"),
+                  num_task_c, num_task))
 
     for dic in task_completed:
         for key in dic:
             if key == "title":
                 value = dic[key]
                 print(f"\t {value}")
-    """
-        for task in task_completed:
-            print("\t {}".format(task.get("title")))
-    """
