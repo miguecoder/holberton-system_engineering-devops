5c693b7c62ff047a673aa36512a8fa1d384bdebc
miguecoder <mmarqzm@gmail.com>
2022-09-09 21:25:57 -0500

    script to export data in the JSON format.

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script to export data in the JSON format"""
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    id = argv[1]
+    all_url = "https://jsonplaceholder.typicode.com/todos/"
+    user_url = "https://jsonplaceholder.typicode.com/users/"
+
+    tasks = requests.get(all_url, params={"userId": id}).json()
+    name = requests.get(user_url + id).json().get("username")
+    filter = {"userId": id, "completed": "true"}
+    task_completed = requests.get(all_url, params=filter).json()
+
+    with open(id + '.json', 'w') as outfile:
+        json.dump({id: [{"task": tt.get("title"), "completed": tt.get(
+            "completed"), "username": name} for tt in tasks]}, outfile)
